41ee22137f0f97bfceffd29a4477658d2d5a155f
Leigh <1142961+leighgordy@users.noreply.github.com>
2026-09-02 21:29:59 +0100

    Made html generation run as parallel processes

--- src/blog-generation/markdown/markdown_html_convertor.ts
+++ src/blog-generation/markdown/markdown_html_convertor.ts
@@ -8,6 +8,8 @@ import {
 } from "./parser/orderedListParser.ts";
 import { tableParse, tableRegex } from "./parser/tableParser.ts";
 import { parseCodeBlocks } from "./parser/codeParser.ts";
+import asyncPool from "../thread-management/async-pool.ts";
+import appConfig from "../../app-config.ts";
 
 /**
  * Converts a markdown file to an HTML file. Follows the CommonMark standard \n\n for new section.
@@ -22,36 +24,33 @@ const markdownHtmlConvertor = async (
   const rawHtml = await parseCodeBlocks(markdown);
 
   const blocks = rawHtml.split(/\n{2,}/);
-  const processedBlocks: string[] = [];
 
-  for (const block of blocks) {
-    const trimmedBlock = block.trim();
-    if (!trimmedBlock) continue;
+  const { maxParallelProcesses } = appConfig;
 
-    if (headerRegex.test(trimmedBlock)) {
-      processedBlocks.push(trimmedBlock.replace(headerRegex, headerParse));
-    } else if (tableRegex.test(trimmedBlock)) {
-      processedBlocks.push(trimmedBlock.replace(tableRegex, tableParse));
-    } else if (bulletListRegex.test(trimmedBlock)) {
-      processedBlocks.push(
-        trimmedBlock.replace(bulletListRegex, bulletListParse),
-      );
-    } else if (orderedListRegex.test(trimmedBlock)) {
-      processedBlocks.push(
-        trimmedBlock.replace(orderedListRegex, orderedListParse),
-      );
-    } else if (imageRegex.test(trimmedBlock)) {
-      processedBlocks.push(
-        trimmedBlock.replace(imageRegex, (_, altText, url) =>
+  const processedBlocks = await asyncPool<string, string>(
+    blocks,
+    maxParallelProcesses,
+    async (block) => {
+      const trimmedBlock = block.trim();
+      if (!trimmedBlock) return "";
+
+      if (headerRegex.test(trimmedBlock)) {
+        return trimmedBlock.replace(headerRegex, headerParse);
+      } else if (tableRegex.test(trimmedBlock)) {
+        return trimmedBlock.replace(tableRegex, tableParse);
+      } else if (bulletListRegex.test(trimmedBlock)) {
+        return trimmedBlock.replace(bulletListRegex, bulletListParse);
+      } else if (orderedListRegex.test(trimmedBlock)) {
+        return trimmedBlock.replace(orderedListRegex, orderedListParse);
+      } else if (imageRegex.test(trimmedBlock)) {
+        return trimmedBlock.replace(imageRegex, (_, altText, url) =>
           imageParse(baseDirectory, _, altText, url),
-        ),
-      );
-    } else {
-      processedBlocks.push(
-        trimmedBlock.replace(paragraphRegex, paragraphParse),
-      );
-    }
-  }
+        );
+      } else {
+        return trimmedBlock.replace(paragraphRegex, paragraphParse);
+      }
+    },
+  );
 
   return processedBlocks.join("\n\n");
 };
